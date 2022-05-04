3439e3dd4f6a7afa8f15197e1e106d39c0e87833
Pavilion <marihd10@gmail.com>
2022-05-03 21:55:52 -0300

    Add files dellanchesback

---
+++ src/main/java/com/nitech/dellanches/DellanchesbackApplication.java
@@ -0,0 +1,13 @@
+package com.nitech.dellanches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DellanchesbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DellanchesbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nitech/dellanches/DellanchesbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitech.dellanches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DellanchesbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
